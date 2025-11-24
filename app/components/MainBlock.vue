7bd580889058b7f6b0c514fdddd9ca2c0fad11e5
Vladyslav Kubyshkin <kubyshkin@brights.io>
2025-11-24 14:38:58 +0200

    Revert "fix(MainBlock): only x1 density to improve image sizes for PageSpeed"
    This reverts commit edf04366acaadf30b19dfbe5a3c44a3798e5ce8c.

--- app/components/MainBlock.vue
+++ app/components/MainBlock.vue
@@ -82,7 +82,6 @@ onBeforeUnmount(() => {
           :height="image.height"
           sizes="xs:262px sm:327px 2sm:506px md:930px lg:501px"
           :preload="{ fetchPriority: 'high' }"
-          densities="x1"
         />
       </div>
       <div class="main-section__right">
