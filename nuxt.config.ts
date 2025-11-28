82f6cf58649f42ec2fb9bb48538e6cb8fb37e39b
Vladyslav Kubyshkin <kubyshkin@brights.io>
2025-11-28 13:36:50 +0200

    Revert "fix(config): disable inlineStyles"
    This reverts commit 4475dc293d8294b8d13516a8ec6d9bc966d6c0a9.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,9 +19,6 @@ const payloadHostname = new URL(payloadUrl).hostname;
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  features: {
-    inlineStyles: false,
-  },
 
   runtimeConfig: {
     public: {
