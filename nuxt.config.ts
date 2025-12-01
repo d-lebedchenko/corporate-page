e9716c24ab9486d4788e606302d8389f25047ff5
Vladyslav Kubyshkin <kubyshkin@brights.io>
2025-12-01 18:23:19 +0200

    fix(config): disable vite.ssr.noExternal

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -77,9 +77,6 @@ export default defineNuxtConfig({
       }),
       vsharp(),
     ],
-    ssr: {
-      noExternal: process.env.NODE_ENV === "production" ? true : undefined,
-    },
     build: {
       cssMinify: "lightningcss",
       minify: "terser",
